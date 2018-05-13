e9d2a57774225f269b4637e55eb364c0627a1449
Mark Cruz <markchriscruz@yahoo.com>
2018-05-13 03:54:55 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,12 +29,13 @@ class Student
     DB[:conn].execute(sql)
   end
   
-  def save
-    sql = <<-SQL
-      INSERT INTO songs (name, grade) 
-      VALUES (?, ?)
-    SQL
-    DB[:conn].execute(sql, self.name, self.grade)
-  end
+  # def save
+  #   sql = <<-SQL 
+  #     INSERT INTO songs (name, grade) 
+  #     VALUES (?, ?)
+  #   SQL
+  #   DB[:conn].execute(sql, self.name, self.grade)
+  # end
   
 end
+
